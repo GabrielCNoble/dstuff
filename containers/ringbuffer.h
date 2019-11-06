ac591bb93391105d1283a8e6e51dd0c309f2ea42
Gabriel Chiarelli Noble <gabrielcnoble@gmail.com>
2019-11-06 11:27:14 -0300

    ringbuffer

---
+++ containers/ringbuffer.h
@@ -0,0 +1,24 @@
+#ifndef RINGBUFFER_H
+#define RINGBUFFER_H
+
+#include <stdint.h>
+
+struct ringbuffer_t
+{
+    void *buffer;
+    uint32_t elem_size;
+    uint32_t buffer_size;
+    uint32_t free_slots;
+    uint32_t next_in;
+    uint32_t next_out;
+};
+
+
+struct ringbuffer_t create_ringbuffer(uint32_t elem_size, uint32_t buffer_size);
+
+uint32_t add_ringbuffer_element(struct ringbuffer_t *ringbuffer, void *element);
+
+void *get_ringbuffer_element(struct ringbuffer_t *ringbuffer);
+
+
+#endif
